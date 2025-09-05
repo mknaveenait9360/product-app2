5eac103c05f2fdae98d01d4a26e50713a7418026
Naveen M K <mknaveenait@gmail.com>
2025-09-05 19:00:16 +0530

    Ennabeled CORS

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { logTokenMiddleware } from './common/middleware/log-token.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.use(logTokenMiddleware); 
+  // Enable CORS
+  app.enableCors({
+    origin: 'http://localhost:4000', // replace with your frontend URL
+    credentials: true, // if you need to send cookies
+  });
+
   await app.listen(3000);
 }
 bootstrap();
